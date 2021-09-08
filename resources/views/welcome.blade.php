8e989442102454b4d47aae2e86472cf19640e396
tonynnabs <tonynnabs01@gmail.com>
2021-09-08 13:16:37 -0400

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,8 @@
             </div>
             <h3 class="mb-5 mt-32 lg:mt-0 lg:ml-10 mx-5 lg:mx-0 text-lg font-bold">Anthony Nnabude</h3>
             <div class="mx-5 lg:mx-0 lg:ml-10 mb-20 xl:mb-0">
-                <h1 class="text-5xl">Fullstack Developer.</h1>
-                <p class="mt-3">Exceptional creative fullstack developer with three year experience in designing, developing and testing multiple web-based applications with an in-depth understanding of web practices.</p>
+                <h1 class="text-5xl">Software Engineer.</h1>
+                <p class="mt-3">Exceptional creative Software Engineer with Five year experience in designing, developing and testing multiple web-based applications with an in-depth understanding of web practices.</p>
                 <ul class="mt-3 flex flex-wrap justify-between items-center">
                     <li><img class="w-10 lg:w-20 filter grayscale" src="/img/laravel.png" alt="Laravel"></li>
                     <li><img class="w-10 lg:w-16 filter grayscale" src="/img/nodejs.png" alt="Node Js"></li>
@@ -21,17 +21,27 @@
 
         </div>
         <div class="h-72 lg:h-full lg:w-1/2 xl:w-2/3 bg-gray-400">
-            <div class="absolute top-5 lg:top-10 right-5 lg:right-10">
+            <div class="absolute top-5 lg:top-10 right-5 z-50 lg:right-10">
                 <div class="text-white flex justify-center items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path class="text-black lg:text-white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                       </svg>
                 </div>
             </div>
-            <img class="w-full h-full object-cover object-top" src="/img/tonynnabs.jpg" alt="Tonynnabs - frontend developer">
+            <div class="w-full h-full bg-pattern flex items-center justify-center relative">
+                <img class="max-w-sm rounded-full border-2 border-yellow-500"   src="/img/profile_picture.jpg" alt="Tonynnabs - Software Engineer">
+                <div class="absolute bottom-5 right-10">
+                    <ul class="flex py-5 lg:py-0 text-white">
+                        <li class="px-3"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></li>
+                        <li class="px-3"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg></li>
+                        <li class="px-3"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg></li>
+                        <li class="px-3"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="flex flex-col lg:flex-row justify-end items-center px-0 lg:px-10 fold-bottom" >
+    {{-- <div class="flex flex-col lg:flex-row justify-end items-center px-0 lg:px-10 fold-bottom" >
         <div class="block lg:absolute left-16 bottom-0 w-full lg:w-4/6 h-1/6">
             <div class="bg-black h-full  w-full text-white flex  flex-col lg:flex-row justify-between items-start lg:items-center px-5 lg:px-10 py-3">
                 <h4 class="font-bold">Currently Building</h4>
@@ -46,13 +56,6 @@
 
             </div>
         </div>
-        <div>
-            <ul class="flex py-5 lg:py-0">
-                <li class="px-3"><img src="/img/github.svg" alt="github"></li>
-                <li class="px-3"><img src="/img/linkedin.svg" alt="LinkedIn"></li>
-                <li class="px-3"><img src="/img/twitter.svg" alt="Twitter"></li>
-                <li class="px-3"><img src="/img/mail.svg" alt="Email"></li>
-            </ul>
-        </div>
-    </div>
+
+    </div> --}}
 </x-guest-layout>
